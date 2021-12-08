1aca1b6c6328abe9d1e5d4ddec59ccdfc9461435
Gregmo07 <laurentg07@gmail.com>
2021-12-08 05:06:11 +0100

    loader

---
+++ app.js
@@ -0,0 +1,7 @@
+const loader = document.querySelector('.loader');
+const accueil = document.querySelector('.accueil');
+window.addEventListener('load', ()=>{
+    loader.classList.add('fondu-out');
+    accueil.classList.add('fondu-in')
+})
+
